5a705007d447e7f08a057d93084e0a854fba75cd
manjunathakullai <30720631+manjunathakullai@users.noreply.github.com>
2024-12-19 12:03:51 +0000

    This is tfstate file example

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0e2c8caa4b6378d8c"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-079f92e1ba5e6c7cf"
+   
+}
